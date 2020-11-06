a2da272da1c5221274a351a1384ba83990d57066
dayothompson <dayo.thompson@yahoo.com>
2020-11-05 23:32:17 -0500

    push

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,7 @@
 console.log("CALLING LOGIC.JS");
 
 // var url = "http://127.0.0.1:5000//jsonified";
-var url = "https://dayo-java.herokuapp.com//jsonified";
+var url = "https://dayo-java.herokuapp.com/jsonified";
 
 // ######## Heat Map ########
 function heatmap() {
